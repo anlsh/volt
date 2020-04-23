fbd66f0400b6906ba01cd2f7c95936810c024b99
Anish Moorthy <anlsh@protonmail.com>
2020-04-23 00:31:26 -0400

    Clean up hash macro to allow lexical binds

--- reader-macros.lisp
+++ reader-macros.lisp
@@ -1,17 +1,14 @@
 ;; Macro for inline hash tables
 (in-package :volt)
 
-;; Following code modified from https://news.ycombinator.com/item?id=1611090
-;; It has serious bugs, yes I know :P
-(defun hash-table-from-pairs (pairs)
-  (let ((table (make-hash-table :test #'equalp)))
-    (mapcar (lambda (pair) (sethash ((eval (first pair)) (eval (second pair))) table))
-            pairs)
-    table))
+(labels ((in-place-hash (stream char)
+           (declare (ignore char))
+           (let ((ls (read-delimited-list #\} stream t)))
+             (with-gensyms (table)
+               `(let ( (,table (make-hash-table :test #'equalp)))
+                  ,@(mapcar (lambda (p) `(setf (gethash ,(car p) ,table) ,(cadr p)))
+                            ls)
+                  ,table)))))
+  (set-macro-character #\{ #'in-place-hash))
 
-(defun in-place-hash (stream char)
-  (declare (ignore char))
-  `(hash-table-from-pairs ',(read-delimited-list #\} stream t)))
-
-(set-macro-character #\{ #'in-place-hash)
 (set-syntax-from-char #\} #\))
