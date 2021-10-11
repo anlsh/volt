57dcc2f7575d0de96f8a178f9bba80f53a3b2d45
Anish Moorthy <anlsh@protonmail.com>
2021-10-10 21:16:07 -0400

    v1 of "pythonic-import" working

--- macros.lisp
+++ macros.lisp
@@ -7,3 +7,31 @@
   (alx:once-only (key table)
     `(unless (second (multiple-value-list (gethash ,key ,table)))
        (sethash (,key ,val) ,table))))
+
+
+(defmacro pythonic-import (imports &rest body)
+  ;; Imports has form
+  ;;     (package-name (symbol-to-import | symbol-to-import nickname)*)*
+  (let ((replacement-map (fset:empty-map)))
+    (arrow-macros:-<> imports
+      (mapcar (lambda (pkg-spec)
+                (let ((pkg-name (car pkg-spec)))
+                  (mapcar (lambda (sym-spec)
+                            (if (symbolp sym-spec)
+                                (list sym-spec
+                                      (find-symbol (symbol-name sym-spec)
+                                                   pkg-name))
+                                (list (car sym-spec)
+                                      (find-symbol (symbol-name (cadr sym-spec))
+                                                   pkg-name))))
+                          (cdr pkg-spec))))
+              arrow-macros:<>)
+      (apply #'append arrow-macros:<>)
+      (mapcar (lambda (x) (fset:adjoinf replacement-map (car x) (cadr x)))
+              arrow-macros:<>))
+    (labels ((replace-symbols (x)
+               (cond ((listp x) (mapcar #'replace-symbols x))
+                     ((symbolp x) (multiple-value-bind (new-sym in?) (fset:@ replacement-map x)
+                                    (if in? new-sym x)))
+                     (t x))))
+      `(progn ,@(replace-symbols body)))))
